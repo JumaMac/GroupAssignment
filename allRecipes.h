23658e5022be0a8ad3f34dd5c68ee319a2cd8968
Jaismin <jaisminks06@gmail.com>
2025-04-02 16:00:07 -0400

    Create allRecipes.h

---
+++ allRecipes.h
@@ -0,0 +1,19 @@
+#ifndef RECIPE_FUNCTIONS_H
+#define RECIPE_FUNCTIONS_H
+
+// Function declarations
+void display_breakfast_recipes();
+void display_lunch_recipes();
+void display_dinner_recipes();
+void display_dessert_recipes();
+void display_veg_vegan_recipes();
+void breakfast_recipe_1(int servings);
+void breakfast_recipe_2(int servings);
+void lunch_recipe_1(int servings);
+void lunch_recipe_2(int servings);
+void dinner_recipe_1(int servings);
+void dinner_recipe_2(int servings);
+void dessert_recipe_1(int servings);
+void dessert_recipe_2(int servings);
+
+#endif // RECIPE_FUNCTIONS_H
